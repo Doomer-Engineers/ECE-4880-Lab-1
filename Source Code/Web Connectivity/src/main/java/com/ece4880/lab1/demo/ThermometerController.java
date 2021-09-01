3ead6cc9ebb09cf09304c395b4d391aa1702a14a
Brenden Taul <89364653+btaul@users.noreply.github.com>
2021-09-01 16:02:45 -0500

    First Merge for Basic File Structure (#1)
    * init spring project and started working on html
    
    * doing stuff for file structure, will squash later
    
    * Delete .mvn/wrapper directory
    
    * Delete src directory
    
    * Delete mvnw
    
    * Delete mvnw.cmd
    
    * Delete pom.xml
    
    * Delete Source Code/Web Connectivity directory
    
    * Delete .gitignore
    
    * Delete README.md
    
    * Add files via upload
    
    * finally got file structure
    
    * now i got it

---
+++ Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
@@ -0,0 +1,13 @@
+package com.ece4880.lab1.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThermometerController {
+
+    @GetMapping("")
+    public String viewHomePage(){
+        return "ThermometerView";
+    }
+}
