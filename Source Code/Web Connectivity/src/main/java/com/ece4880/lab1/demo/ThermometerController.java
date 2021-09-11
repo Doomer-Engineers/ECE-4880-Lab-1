666b99f8260aaa6db2de5eeddfceb651585d65d5
henry-bulluck <thbulluck@gmail.com>
2021-09-11 16:33:48 -0500

    Added Twilio dependency to Maven

--- Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
+++ Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
@@ -2,6 +2,9 @@ package com.ece4880.lab1.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
 @Controller
 public class ThermometerController {
