16a78625c6987dc6155b24c5e9472bf2d99fdb42
henry-bulluck <thbulluck@gmail.com>
2021-09-11 17:37:40 -0500

    Added function to send message if thermometer limits are reached

--- Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
+++ Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
@@ -12,12 +12,12 @@ public class ThermometerController {
     public static final String ACCOUNT_SID = "AC088384fe5a644619e6991093ed038b2f";
     public static final String AUTH_TOKEN = "ccb4ebaaf2ec8cc5f9e09c78defad059";
 
-    public static final String TO_NUMBER = "+14145269642";
+    public static String to_number = "+14145269642";
     public static final String FROM_NUMBER = "+14088374565";
 
     // Main method to test text output
     public static void main(String[] args) {
-        sendMessage("Hello world.");
+//        sendMessage("Hello world.");
     }
 
     // Method that sends a given message to the TO_NUMBER
@@ -25,12 +25,22 @@ public class ThermometerController {
     public static void sendMessage(String my_message){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber(TO_NUMBER),   // to number
+                        new com.twilio.type.PhoneNumber(to_number),   // to number
                         new com.twilio.type.PhoneNumber(FROM_NUMBER), // from number
                         my_message)                                   // message
                 .create();
 
         System.out.println(message.getSid());
+    }
+
+    // Code to send text based on temperature booleans
+    public static void textTempLimits(boolean maxTemp, boolean minTemp){
+        if(maxTemp){
+            sendMessage("Maximum temperature reached");
+        }
+        else if(minTemp){
+            sendMessage("Minimum temperature reached");
+        }
     }
 
     @GetMapping("")
