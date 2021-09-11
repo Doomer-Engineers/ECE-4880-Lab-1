843de59829d15a3bd3c408e4362641d226c717fc
henry-bulluck <thbulluck@gmail.com>
2021-09-11 16:55:16 -0500

    Created main function to test sending texts

--- Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
+++ Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
@@ -9,10 +9,25 @@ import com.twilio.type.PhoneNumber;
 @Controller
 public class ThermometerController {
 
+    public static final String ACCOUNT_SID = "AC088384fe5a644619e6991093ed038b2f";
+    public static final String AUTH_TOKEN = "ccb4ebaaf2ec8cc5f9e09c78defad059";
+
+    // Main method to test text output
+    // Uses example code from Twilio quickstart documentation
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                        new com.twilio.type.PhoneNumber("+14145269642"), // to number
+                        new com.twilio.type.PhoneNumber("+14088374565"), // from number
+                        "Guess who got the code texting :)")       // message
+                .create();
+
+        System.out.println(message.getSid());
+    }
+
     @GetMapping("")
     public String viewHomePage(){
         return "ThermometerView";
     }
 
-    //Test comment
 }
