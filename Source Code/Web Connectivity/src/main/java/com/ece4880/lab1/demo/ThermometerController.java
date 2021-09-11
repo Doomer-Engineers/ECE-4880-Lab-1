24d92f4995cee919ece448dccd82988ef00b93bf
henry-bulluck <thbulluck@gmail.com>
2021-09-11 17:09:45 -0500

    Moved texting code to a reusable method sendMessage

--- Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
+++ Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
@@ -12,14 +12,22 @@ public class ThermometerController {
     public static final String ACCOUNT_SID = "AC088384fe5a644619e6991093ed038b2f";
     public static final String AUTH_TOKEN = "ccb4ebaaf2ec8cc5f9e09c78defad059";
 
+    public static final String TO_NUMBER = "+14145269642";
+    public static final String FROM_NUMBER = "+14088374565";
+
     // Main method to test text output
-    // Uses example code from Twilio quickstart documentation
     public static void main(String[] args) {
+        sendMessage("Hello world.");
+    }
+
+    // Method that sends a given message to the TO_NUMBER
+    // Uses example code from Twilio quickstart documentation
+    public static void sendMessage(String my_message){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber("+14145269642"), // to number
-                        new com.twilio.type.PhoneNumber("+14088374565"), // from number
-                        "Guess who got the code texting :)")       // message
+                        new com.twilio.type.PhoneNumber(TO_NUMBER),   // to number
+                        new com.twilio.type.PhoneNumber(FROM_NUMBER), // from number
+                        my_message)                                   // message
                 .create();
 
         System.out.println(message.getSid());
