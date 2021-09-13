9c60443816111f9c87b7e0f320fd10ec7473d1ab
Brenden Taul <89364653+btaul@users.noreply.github.com>
2021-09-12 22:17:05 -0500

    Squashed commit of the following:
    commit c8e5624304c4a000a443091c1b5d6a66db9da251
    Author: henry-bulluck <thbulluck@gmail.com>
    Date:   Sat Sep 11 17:40:46 2021 -0500
    
        Moved main method
    
    commit d4a04a43f5b13e58a25436e51ef72968409c23b1
    Author: henry-bulluck <thbulluck@gmail.com>
    Date:   Sat Sep 11 17:40:02 2021 -0500
    
        Added getter and setter for the number we will be texting
    
    commit 16a78625c6987dc6155b24c5e9472bf2d99fdb42
    Author: henry-bulluck <thbulluck@gmail.com>
    Date:   Sat Sep 11 17:37:40 2021 -0500
    
        Added function to send message if thermometer limits are reached
    
    commit 24d92f4995cee919ece448dccd82988ef00b93bf
    Author: henry-bulluck <thbulluck@gmail.com>
    Date:   Sat Sep 11 17:09:45 2021 -0500
    
        Moved texting code to a reusable method sendMessage
    
    commit 843de59829d15a3bd3c408e4362641d226c717fc
    Author: henry-bulluck <thbulluck@gmail.com>
    Date:   Sat Sep 11 16:55:16 2021 -0500
    
        Created main function to test sending texts
    
    commit 666b99f8260aaa6db2de5eeddfceb651585d65d5
    Author: henry-bulluck <thbulluck@gmail.com>
    Date:   Sat Sep 11 16:33:48 2021 -0500
    
        Added Twilio dependency to Maven
    
    commit d579a0b9924833398d108f945933f32f2f41c75f
    Author: henry-bulluck <thbulluck@gmail.com>
    Date:   Fri Sep 10 13:52:03 2021 -0500
    
        Added comment to test branching

--- Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
+++ Source Code/Web Connectivity/src/main/java/com/ece4880/lab1/demo/ThermometerController.java
@@ -6,17 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
 @Controller
 public class ThermometerController {
 
-    static final String HOMEPAGE = "ThermometerView";
-    static final String WEATHER_ATTRIBUTES = "weatherAttributes";
+    private static final String HOMEPAGE = "ThermometerView";
+    private static final String WEATHER_ATTRIBUTES = "weatherAttributes";
+
+    private static final String ACCOUNT_SID = "AC088384fe5a644619e6991093ed038b2f";
+    private static final String AUTH_TOKEN = "ccb4ebaaf2ec8cc5f9e09c78defad059";
+
+    private static String to_number = "+14145269642";
+    private static final String FROM_NUMBER = "+14088374565";
 
     @ModelAttribute(WEATHER_ATTRIBUTES)
     public WeatherAttributes weatherAttributesDTO() {
-        return new WeatherAttributes();
-    }
+        return new WeatherAttributes(); }
 
     @GetMapping("")
     public String viewHomePage(@ModelAttribute(WEATHER_ATTRIBUTES) WeatherAttributes weatherAttributes, Model model){
@@ -28,6 +36,22 @@ public class ThermometerController {
     public String updateValues(@ModelAttribute(WEATHER_ATTRIBUTES) WeatherAttributes weatherAttributes, Model model){
         weatherAttributes.setPhoneNumber(weatherAttributes.getPhoneNumberString());
         weatherAttributes.updateTemps();
+
+        boolean isMaxTemp = false;
+        boolean isMinTemp = false;
+        
+        int temp = 50;
+
+        if (temp > weatherAttributes.getMaxTemp()){
+            isMaxTemp = true;
+        }
+
+        if (temp < weatherAttributes.getMinTemp()){
+            isMinTemp = true;
+        }
+
+        textTempLimits(isMaxTemp,isMinTemp);
+
         System.out.println();
         System.out.println("Phone Number: " + weatherAttributes.getPhoneNumber());
         System.out.println("Max Temp: " + weatherAttributes.getMaxTemp());
@@ -35,6 +59,40 @@ public class ThermometerController {
         System.out.println("Current State: " + weatherAttributes.getDegrees());
         model.addAttribute(weatherAttributes);
         return HOMEPAGE;
+    }
+
+        //getter and setter for the number to text
+    public static String getTo_number() {
+        return to_number;
+    }
+    public static void setTo_number(String to_number) {
+        ThermometerController.to_number = to_number;
+    }
+
+    // Method that sends a given message to the TO_NUMBER
+    // Uses example code from Twilio quickstart documentation
+    public static void sendMessage(String my_message){
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                        new com.twilio.type.PhoneNumber(to_number),   // to number
+                        new com.twilio.type.PhoneNumber(FROM_NUMBER), // from number
+                        my_message)                                   // message
+                .create();
+
+        System.out.println(message.getSid());
+    }
+
+    // Code to send text based on temperature booleans
+    public static void textTempLimits(boolean isMaxTemp, boolean isMinTemp){
+        if(isMaxTemp && !isMinTemp){
+            sendMessage("Maximum temperature reached");
+        }
+        else if(isMinTemp && !isMaxTemp){
+            sendMessage("Minimum temperature reached");
+        }
+        else(isMaxTemp && isMinTemp){
+            sendMessage("Temperature reached")
+        }
     }
 
 }
